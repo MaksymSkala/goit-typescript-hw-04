509b31455616338fa25c5fb3b361b5fd7391f6aa
MaksymSkala <maksymskala@gmail.com>
2023-12-05 00:19:46 +0200

    Add

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,91 +1,11 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
-import noop from "lodash/noop";
+import { useState, ChangeEvent } from "react";
 
-type MenuIds = "first" | "second" | "last";
-type Menu = { id: MenuIds; title: string };
+export function FormComponent() {
+  const [value, setValue] = useState("");
 
-// Додати тип Menu Selected
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
 
-const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
-});
-
-// Додайте тип MenuAction
-
-const MenuActionContext = createContext<MenuAction>({
-  onSelectedMenu: noop,
-});
-
-type PropsProvider = {
-  children; // Додати тип для children
-};
-
-function MenuProvider({ children }: PropsProvider) {
-  // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
-
-  const menuContextAction = useMemo(
-    () => ({
-      onSelectedMenu: setSelectedMenu,
-    }),
-    []
-  );
-
-  const menuContextSelected = useMemo(
-    () => ({
-      selectedMenu,
-    }),
-    [selectedMenu]
-  );
-
-  return (
-    <MenuActionContext.Provider value={menuContextAction}>
-      <MenuSelectedContext.Provider value={menuContextSelected}>
-        {children}
-      </MenuSelectedContext.Provider>
-    </MenuActionContext.Provider>
-  );
-}
-
-type PropsMenu = {
-  menus; // Додайте вірний тип для меню
-};
-
-function MenuComponent({ menus }: PropsMenu) {
-  const { onSelectedMenu } = useContext(MenuActionContext);
-  const { selectedMenu } = useContext(MenuSelectedContext);
-
-  return (
-    <>
-      {menus.map((menu) => (
-        <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
-        </div>
-      ))}
-    </>
-  );
-}
-
-export function ComponentApp() {
-  const menus: Menu[] = [
-    {
-      id: "first",
-      title: "first",
-    },
-    {
-      id: "second",
-      title: "second",
-    },
-    {
-      id: "last",
-      title: "last",
-    },
-  ];
-
-  return (
-    <MenuProvider>
-      <MenuComponent menus={menus} />
-    </MenuProvider>
-  );
+  return <input type="text" value={value} onChange={handleChange} />;
 }
